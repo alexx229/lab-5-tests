495880406375f23de1ef4026f736b270301bb22e
olakowall <k.magdalena.aleksandra@gmail.com>
2023-07-17 20:39:22 +0200

    Lab5-NUnit-Tests

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testy_lab5;
+
+[ApiController]
+[Route("api/tasks")]
+public class TaskController : ControllerBase
+{
+    private readonly TaskManager _taskManager;
+
+    public TaskController(TaskManager taskManager)
+    {
+        _taskManager = taskManager;
+    }
+
+    [HttpPost]
+    public IActionResult CreateTask(testy_lab5.Task task)
+    {
+        _taskManager.AddTask(task);
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult GetAllTasks()
+    {
+        var tasks = _taskManager.GetTasks();
+        return Ok(tasks);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetTaskById(int id)
+    {
+        var task = _taskManager.GetTaskById(id);
+        if (task != null)
+        {
+            return Ok(task);
+        }
+        return NotFound();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateTask(int id, testy_lab5.Task updatedTask)
+    {
+        var result = _taskManager.UpdateTask(id, updatedTask);
+        if (result)
+        {
+            return Ok();
+        }
+        return NotFound();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteTask(int id)
+    {
+        var result = _taskManager.DeleteTask(id);
+        if (result)
+        {
+            return Ok();
+        }
+        return NotFound();
+    }
+}
